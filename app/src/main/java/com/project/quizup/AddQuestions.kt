476840de20878b66bb1de267a6414d1a4f76b12e
Omnia Masoud <omniamasoud@Omnias-Air.MissouriState.edu>
2020-03-06 09:15:02 -0600

    Initial commit

---
+++ app/src/main/java/com/project/quizup/AddQuestions.kt
@@ -0,0 +1,12 @@
+package com.project.quizup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddQuestions : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_questions)
+    }
+}
---
+++ app/src/main/java/com/project/quizup/NotesListActivity.kt
@@ -0,0 +1,15 @@
+package com.project.quizup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotesListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes_list)
+
+
+
+    }
+}
