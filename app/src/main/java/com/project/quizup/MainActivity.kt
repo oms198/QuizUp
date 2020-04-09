330210247ec900d0eb4b010078bd3ae744800665
MooreKegan <Keganmoore12@gmail.com>
2020-04-09 14:35:40 -0500

    added splash functionality, homepage to questions page button, and add questions functionality

--- app/src/main/java/com/project/quizup/MainActivity.kt
+++ app/src/main/java/com/project/quizup/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.project.quizup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    //button variables
+    private lateinit var addQuestionsButton: Button
+    private lateinit var notesButton: Button
+    private lateinit var flashcardsButton: Button
+    private lateinit var quizButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //assign buttons
+        addQuestionsButton = findViewById(R.id.quiz_button)
+        notesButton = findViewById(R.id.notes_button)
+        flashcardsButton = findViewById(R.id.flashcard_button)
+        quizButton = findViewById(R.id.take_quiz_button)
+
+        //add questions button is pressed, go to that page
+        addQuestionsButton.setOnClickListener{
+            val intent = Intent(this, AddQuestions::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/project/quizup/SplashScreenActivity.kt
+++ app/src/main/java/com/project/quizup/SplashScreenActivity.kt
@@ -1,15 +1,21 @@
 package com.project.quizup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashTimeOut:Long = 3000 //3 second delay
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
-       /* implementation of splash screen in the next prototype */
-
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashTimeOut)
     }
 }
