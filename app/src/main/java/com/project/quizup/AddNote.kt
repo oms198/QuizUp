988d7595fde08e95c380de40b24bd7d9efc32ac3
omniamasoud <omniyamagdi@gmail.com>
2020-04-20 14:53:48 -0500

    "nothing major- what was submitted as the vertical prototype"

--- app/src/main/java/com/project/quizup/AddNote.kt
+++ app/src/main/java/com/project/quizup/AddNote.kt
@@ -4,17 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.widget.Toolbar
 
+
+//need to connect with database to enable the functionalities
 class AddNote : AppCompatActivity() {
 
     private lateinit var homeButton: Button
+    //private lateinit var toolbar: Toolbar
+    //private lateinit var noteTitle: EditText
+    //private lateinit var noteDetails: EditText
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
 
-        homeButton = findViewById(R.id.home_button)
+        //toolbar = findViewById(R.id.toolbar)
+        //setSupportActionBar(toolbar)
 
+        //noteTitle = findViewById(R.id.noteTitle)
+        //noteDetails = findViewById(R.id.noteDetails)
+
+        homeButton = findViewById(R.id.home_button)
         homeButton.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
--- app/src/main/java/com/project/quizup/NotesListActivity.kt
+++ app/src/main/java/com/project/quizup/NotesListActivity.kt
@@ -2,14 +2,21 @@ package com.project.quizup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
 
 class NotesListActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var recyclerView:RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes_list)
 
-
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        recyclerView= findViewById(R.id.listOfNotes)
 
     }
 }
